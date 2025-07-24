79b2010615ff51ea3dd2786f69dfc0c187bbb91b
Fikri <mfikria@gmail.com>
2025-07-24 23:01:11 +0700

    fix: serve asset in github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/github-search',
+  assetPrefix: '/github-search',
   images: {
     unoptimized: true
   }
